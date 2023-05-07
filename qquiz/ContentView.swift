183007350482d66dda9ed83fe2f5eba3f4ee3fe0
Donizetti Aparecido de Souza Junior <71847472+souza10v@users.noreply.github.com>
2023-05-07 10:05:35 -0300

    Initial Commit

---
+++ qquiz/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  qquiz
+//
+//  Created by Donizetti de Souza on 5/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ qquiz/qquizApp.swift
@@ -0,0 +1,17 @@
+//
+//  qquizApp.swift
+//  qquiz
+//
+//  Created by Donizetti de Souza on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct qquizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
